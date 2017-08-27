e309a8e4c093ef633a34cca747b5751b67413489
peterwmwong <peter.wm.wong@gmail.com>
2017-08-27 10:49:04 -0500

    Switch fs implementation to reduce memory usage by 5x.

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
 'use strict';
 
-const START_TIME = Date.now();
-const OUTPUT_THRESHOLD = 200;
-let last_output = 0;
-let bytes = 0;
-let lines = 0;
+const fs = require('fs');
+const { EOL } = require('os');
+const { Buffer } = require('buffer');
 
-const countLines = str => {
+// Can be customized to optimize for memory usage or cpu usage.
+// Higher => ^ Memory v CPU
+// Lower  => v Memory ^ CPU
+const BUFFER_SIZE = 1024 * 10;
+const LINE_DELIMITER = EOL.charCodeAt(0);
+const OUTPUT_THRESHOLD_MS = 200;
+const START_TIME_MS = Date.now();
+
+const countLines = (buffer, size) => {
   let pos = 0;
   let count = 0;
-  while ((pos = (str.indexOf('\n', pos) + 1)) !== 0) count++;
+  // Node's Buffer.p.indexOf() implementation is supa fast/efficient as it uses
+  // libc's memchr.  Unfortunately, it doesn't seem like Ruby MRI or Crystal
+  // use this for index().
+  while ((pos = (buffer.indexOf(LINE_DELIMITER, pos) + 1)) !== 0
+         && pos <= size) count++;
   return count;
 }
 
-const output = (duration_ms, bytes, lines) => {
+const output = (duration_ms, num_bytes, num_lines) => {
   const duration_s = (duration_ms / 1000) | 0;
-  const rate = ((bytes / duration_s / 1024) | 0).toLocaleString();
-  const line_rate = ((lines / duration_s) | 0).toLocaleString();
-  process.stderr.write(`\r\x1b[K${duration_s.toLocaleString()} seconds | ${bytes.toLocaleString()} bytes [ ${rate} kb/sec ] | ${lines.toLocaleString()} lines [ ${line_rate} lines/sec ]`);
+  const rate = ((num_bytes / duration_s / 1024) | 0).toLocaleString();
+  const line_rate = ((num_lines / duration_s) | 0).toLocaleString();
+  process.stderr.write(`\r\x1b[K${duration_s.toLocaleString()} seconds | ${num_bytes.toLocaleString()} bytes [ ${rate} kb/sec ] | ${num_lines.toLocaleString()} lines [ ${line_rate} lines/sec ]`);
 }
 
-process.stdin.on('data', data => {
+const stdin = fs.openSync('/dev/stdin', 'r');
+const stdout = fs.openSync('/dev/stdout', 'w');
+const buf = Buffer.allocUnsafe(BUFFER_SIZE);
+
+let last_output = 0;
+let bytes = 0;
+let lines = 0;
+let num = 0;
+
+while((num = fs.readSync(stdin, buf, 0, BUFFER_SIZE)) > 0) {
+  fs.writeSync(stdout, buf, 0, num);
+
   const time = Date.now();
-  bytes += data.length;
-  lines += countLines(data);
+  bytes += num;
+  lines += countLines(buf, num);
 
-  if (time - last_output > OUTPUT_THRESHOLD) {
+  if (time - last_output > OUTPUT_THRESHOLD_MS) {
     last_output = time;
-    output(time - START_TIME, bytes, lines);
+    output(time - START_TIME_MS, bytes, lines);
   }
-});
+}
 
-process.stdin.pipe(process.stdout);
+fs.closeSync(stdin);
+fs.closeSync(stdout);
